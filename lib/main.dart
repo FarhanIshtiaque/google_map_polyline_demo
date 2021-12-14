9ec699a0282ff19f20090db245f7278d11394122
Farhan <rexfarhan@gmail.com>
2021-12-14 17:43:03 +0600

    Polyline marker

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,15 @@ class MapSample extends StatefulWidget {
 class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
 
+
+  Set<Marker> _markers = Set<Marker>();
+  Set<Polygon> _polygons = Set<Polygon>();
+  List<LatLng> polygonLatLngs = <LatLng>[];
+  int _polygonIdCounter =1;
+
+
+
+
   static final CameraPosition _kGooglePlex = const CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
@@ -35,21 +44,53 @@ class MapSampleState extends State<MapSample> {
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
+ @override
+ void initState() {
+    // TODO: implement initState
+    super.initState();
+    _setMarker(LatLng(37.43296265331129, -122.08832357078792));
+    
+  }
+  void _setMarker(LatLng point){
+   _markers.add(Marker(markerId: const MarkerId('marker'),
+     position:point
+   ));
+  }
+  void _setPolygon(){
+   final String polygonIdVal = 'polygon_$_polygonIdCounter';
+   _polygonIdCounter++;
+   _polygons.add(Polygon(polygonId:PolygonId(polygonIdVal),
+     points: polygonLatLngs,
+     strokeWidth: 2,
+     strokeColor: Colors.blue,
+     fillColor: Colors.red.withOpacity(.4)
+
+   ));
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        polygons: _polygons,
+        markers: _markers,
+        mapType: MapType.satellite,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
+        onTap: (point){
+          setState(() {
+            polygonLatLngs.add(point);
+            _setPolygon();
+
+          });
+
+
+        },
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
+
     );
   }
 
